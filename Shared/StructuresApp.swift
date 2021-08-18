ddaa51728580bf1c08b82668edb83da523959ba5
Igor Yaskin <yaskinigorek@icloud.com>
2021-08-18 11:52:00 +0300

    Initial Commit

---
+++ Shared/StructuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  StructuresApp.swift
+//  Shared
+//
+//  Created by Игорь Яськин on 18.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct StructuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
